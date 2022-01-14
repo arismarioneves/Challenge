60a5fc35e6c7a82ac58c1d5618e9fa3866d88e42
Arismário Neves <arismarioneves@users.noreply.github.com>
2022-01-14 20:51:02 -0300

    Challenge2

---
+++ me.java
@@ -0,0 +1,14 @@
+/**
+
+    ▓▓▓▓Dev by Arismário Neves▓▓▓▓
+    arismarioneves@hotmail.com.br
+
+    Some info about me...
+*/
+
+class info {
+    public static void main(String[] args) {
+        System.out.printf("Info:\n------------------------------\n");
+        System.out.println("Name: Arismario Neves \nCountry: Brasilian\nDream: My goal is always to do/be the best I can in all areas of my life.");
+    }
+}
